0023cadc363952172c6965c714953e777d3f9482
Medhy Vinceslas <vinceslas.medhy@gmail.com>
2022-10-24 18:32:47 +0200

    add FVMC and EVMC prediction

--- 3.monte_carlo.py
+++ 3.monte_carlo.py
@@ -9,6 +9,56 @@ Algorithms for learning simultanously from Sequential and Evaluative feedbacks
 """
 
 
+def mc_prediction(π, env, gamma=1., init_lr=.5, min_lr=.01, lr_decay_ratio=.3, n_episodes=500, max_steps=100, mode="FV"):
+    """
+    We start with init_lr and decay its value down to min_lr within the first lr_decay_ratio*100 %
+    of n_episodes.
+    The algorithm works for first and every visit monte carlo.
+
+    Goal is to evaluate the value funtion.
+
+    - mode : FV or EV for First-Visit or Every-Visit Monte carlo
+    """
+    nS = env.observation_space.n
+
+    # Pre-compute the discount factors / learning rates
+    discounts = np.logspace( 0, max_steps, num=max_steps, base=gamma, endpoint=False)
+    lrs = _decay_schedule(init_lr, min_lr, lr_decay_ratio, n_episodes)
+
+    # State-value function
+    V = np.zeros(nS)
+    V_track = np.zeros((n_episodes, nS))
+
+    # for each episode, collect experience
+    for e in range(n_episodes):
+        trajectory = _generate_trajectory(π, env, max_steps)
+        visited = np.zeros(nS, dtype=np.bool)
+
+        # for each experience
+        for t, (state, _, reward, _, _) in enumerate(trajectory):
+            state_has_been_visited = visited[state]  # true or false
+
+            if state_has_been_visited and mode == "FV":
+                # do not compute anything else if visited and FVMC is applied
+                continue
+
+            n_steps = len(trajectory[:t])
+
+            future_rewards = trajectory[t:, 2]  # rewards from this point until the end
+            G = np.sum(discounts[:n_steps] * future_rewards)
+
+            error_estimate = (G - V[state])
+            V[state] = V[state] + lrs[e] * error_estimate
+        
+        V_track[e] = V  # store the value of the entire episode for stats
+        
+    return V.copy(), V_track
+                
+
+    
+
+
+
 def _decay_schedule(init_value, min_value, decay_ratio, max_steps, log_start=-2, log_base=10):
     """Learning rate decay algorithm for learning"""
     decay_steps = int(max_steps * decay_ratio)
@@ -21,14 +71,14 @@ def _decay_schedule(init_value, min_value, decay_ratio, max_steps, log_start=-2,
     return values
 
 
-def generate_trajectory(pi, env, max_steps=20):
+def _generate_trajectory(π, env, max_steps=20):
     """Generate set of experience tuple"""
     done, trajectory = False, []
     while not done:
         state = env.reset()
 
         for t in count():  # equivalent of a while True : t+=1
-            action = pi(state)
+            action = π(state)
             next_state, reward, done, _ = env.step(action)
 
             experience = (state, action, reward, next_state, done)
