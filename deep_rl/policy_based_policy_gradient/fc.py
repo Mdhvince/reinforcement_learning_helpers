bee226c981bf7b7ee9776c82e1c93f61eed7487c
Medhy Vinceslas <vinceslas.medhy@gmail.com>
2022-11-11 17:00:19 +0100

    implement Vanilla Policy Gradient

---
+++ deep_rl/policy_based_policy_gradient/fc.py
@@ -0,0 +1,144 @@
+import warnings ; warnings.filterwarnings('ignore')
+
+import numpy as np
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+"""Policy Based
+
+Goal is to maximize the true value function of a parameterized policy from all initial states.
+So maximize the true value function by changing the policy (without touching the V-Fucntion)
+
+So we want to find the gradient which will help reaching this objective
+From a trajectory τ, we obtain at each step:
+
+>>>> Gt(τ) ▽θ log πθ(At|St)
+
+For the entire trajectory, we just sum over each step from t=0 to T:
+>>>> sum( Gt(τ) ▽θ log πθ(At|St) )
+
+So the gradient we are trying to estimate is:
+
+>>>> ▽θ J(θ) = sum( Gt(τ) ▽θ log πθ(At|St) )
+
+In plain words:
+- We sample a trajectory
+- For each step, we calculate the return from that step
+- We use the return from step t to weight the probability of the action taken at step t
+
+That mean if the return is bad at time t, it is because action taken at time t was bad
+so by multiplying the bad return with the probability of that action, we reduce the likelihood
+of that action being selected at that step.
+"""
+
+
+class FCDAP(nn.Module):  # Fully connected discret action policy
+    """
+    """
+
+    def __init__(self, device, in_dim, out_dim, hidden_dims=(32, 32), activation=F.relu) -> None:
+        """
+        """
+        super(FCDAP, self).__init__()
+
+        self.device = device
+        self.activation = activation
+
+        self.fc1 = nn.Linear(in_dim, hidden_dims[0])
+        self.hidden_layers = nn.ModuleList()
+
+        for i in range(len(hidden_dims)-1):
+            hidden_layer = nn.Linear(hidden_dims[i], hidden_dims[i+1])
+            self.hidden_layers.append(hidden_layer)
+        
+        self.out_layer = nn.Linear(hidden_dims[-1], out_dim)
+        self.to(self.device)
+
+    def _format(self, x):
+        """
+        Convert state to tensor if not and shape it correctly for the training process
+        """
+        if not isinstance(x, torch.Tensor):
+            x = torch.tensor(x, device=self.device, dtype=torch.float32)
+            x = x.unsqueeze(0)
+        return x
+
+    def forward(self, state):
+        x = self._format(state)
+        x = self.activation(self.fc1(x))
+
+        for fc_hidden in self.hidden_layers:
+            x = self.activation(fc_hidden(x))
+        
+        x = self.out_layer(x)
+        return x
+    
+    def full_pass(self, state):
+        logits = self.forward(state)  # preferences over actions
+
+        # sample action from the probability distribution
+        dist = torch.distributions.Categorical(logits=logits)
+        action = dist.sample()
+
+        log_p_action = dist.log_prob(action).unsqueeze(-1)
+        
+        # the entropy term encourage having evenly distributed actions
+        entropy = dist.entropy().unsqueeze(-1)
+
+        return action.item(), log_p_action, entropy
+    
+    def select_action(self, state):
+        """Helper function for when we just need to sample an action"""
+        logits = self.forward(state)
+        dist = torch.distributions.Categorical(logits=logits)
+        action = dist.sample()
+        return action.item()
+    
+    def select_greedy_action(self, state):
+        logits = self.forward(state)
+        return np.argmax(logits.detach().numpy())
+
+
+
+class FCV(nn.Module):  # Fully connected value (state-value)
+    """
+    """
+
+    def __init__(self, device, in_dim, hidden_dims=(32, 32), activation=F.relu) -> None:
+        """
+        """
+        super(FCV, self).__init__()
+
+        self.device = device
+        self.activation = activation
+
+        self.fc1 = nn.Linear(in_dim, hidden_dims[0])
+        self.hidden_layers = nn.ModuleList()
+
+        for i in range(len(hidden_dims)-1):
+            hidden_layer = nn.Linear(hidden_dims[i], hidden_dims[i+1])
+            self.hidden_layers.append(hidden_layer)
+        
+        self.out_layer = nn.Linear(hidden_dims[-1], 1)
+        self.to(self.device)
+
+    def _format(self, x):
+        if not isinstance(x, torch.Tensor):
+            x = torch.tensor(x, device=self.device, dtype=torch.float32)
+            x = x.unsqueeze(0)
+        return x
+
+    def forward(self, state):
+        x = self._format(state)
+        x = self.activation(self.fc1(x))
+
+        for fc_hidden in self.hidden_layers:
+            x = self.activation(fc_hidden(x))
+        
+        x = self.out_layer(x)
+        return x
+
+
+if __name__ == "__main__":
+    pass
--- deep_rl/policy_based_policy_gradient/reinforce.py
+++ deep_rl/policy_based_policy_gradient/reinforce.py
@@ -6,9 +6,9 @@ import warnings ; warnings.filterwarnings('ignore')
 import gym
 import numpy as np
 import torch
-from torch import nn
 import torch.optim as optim
-import torch.nn.functional as F
+
+from fc import FCDAP
 
 """Policy Based
 
@@ -36,71 +36,6 @@ That mean if the return is bad at time t, it is because action taken at time t w
 so by multiplying the bad return with the probability of that action, we reduce the likelihood
 of that action being selected at that step.
 """
-
-
-class FCDAP(nn.Module):  # Fully connected discret action policy
-    """
-    """
-
-    def __init__(self, device, in_dim, out_dim, hidden_dims=(32, 32), activation=F.relu) -> None:
-        """
-        """
-        super(FCDAP, self).__init__()
-
-        self.device = device
-        self.activation = activation
-
-        self.fc1 = nn.Linear(in_dim, hidden_dims[0])
-        self.hidden_layers = nn.ModuleList()
-
-        for i in range(len(hidden_dims)-1):
-            hidden_layer = nn.Linear(hidden_dims[i], hidden_dims[i+1])
-            self.hidden_layers.append(hidden_layer)
-        
-        self.out_layer = nn.Linear(hidden_dims[-1], out_dim)
-        self.to(self.device)
-
-    def _format(self, x):
-        """
-        Convert state to tensor if not and shape it correctly for the training process
-        """
-        if not isinstance(x, torch.Tensor):
-            x = torch.tensor(x, device=self.device, dtype=torch.float32)
-            x = x.unsqueeze(0)
-        return x
-
-    def forward(self, state):
-        x = self._format(state)
-        x = self.activation(self.fc1(x))
-
-        for fc_hidden in self.hidden_layers:
-            x = self.activation(fc_hidden(x))
-        
-        x = self.out_layer(x)
-        return x
-    
-    def full_pass(self, state):
-        logits = self.forward(state)  # preferences over actions
-
-        # sample action from the probability distribution
-        dist = torch.distributions.Categorical(logits=logits)
-        action = dist.sample()
-
-        log_p_action = dist.log_prob(action).unsqueeze(-1)
-        entropy = dist.entropy().unsqueeze(-1)
-
-        return action.item(), log_p_action, entropy
-    
-    def select_action(self, state):
-        """Helper function for when we just need to sample an action"""
-        logits = self.forward(state)
-        dist = torch.distributions.Categorical(logits=logits)
-        action = dist.sample()
-        return action.item()
-    
-    def select_greedy_action(self, state):
-        logits = self.forward(state)
-        return np.argmax(logits.detach().numpy())
 
 
 class Reinforce():
@@ -179,8 +114,6 @@ class Reinforce():
 
 
 
-
-
 if __name__ == "__main__":
 
     EVALUATE_ONLY = True
@@ -192,17 +125,12 @@ if __name__ == "__main__":
         
     nS, nA = env.observation_space.shape[0], env.action_space.n
 
-    ENV_CONF = {
-        "nS": nS, "nA": nA
-    }
-    AGENT_CONF = {
-        "memory_capacity": 50000
-    }
+    ENV_CONF = { "nS": nS, "nA": nA }
     TRAIN_CONF = {
-        "seed": 0, "batch_size": 64, "gamma": .99, "lr": 0.0005,
-        "n_episodes": 5000,
+        "seed": 0, "gamma": .99, "lr": 0.0005, "n_episodes": 5000, "goal_mean_100_reward": 475,
         "device": torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     }
+
     goal_mean_100_reward = 475
 
     # Monte-Carlo reinforce
@@ -218,8 +146,8 @@ if __name__ == "__main__":
 
         evaluation_scores = deque(maxlen=100)
 
-        for i_episode in range(1, TRAIN_CONF["n_episodes"] + 1):
-            state, is_terminal = env.reset(seed=TRAIN_CONF["seed"])[0], False
+        for i_episode in range(1, TRAIN_CONF.n_episodes + 1):
+            state, is_terminal = env.reset(seed=TRAIN_CONF.seed)[0], False
 
             agent.reset_metrics()
             for t_step in count():
@@ -236,7 +164,7 @@ if __name__ == "__main__":
                 mean_100_eval_score = np.mean(evaluation_scores)
                 print(f"Episode {i_episode}\tAverage mean 100 eval score: {mean_100_eval_score}")
 
-                if(mean_100_eval_score >= goal_mean_100_reward):
+                if(mean_100_eval_score >= TRAIN_CONF.goal_mean_100_reward):
                     torch.save(agent.policy.state_dict(), model_path)
                     break
 
