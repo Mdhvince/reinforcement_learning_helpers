26ffb45f490b6ea4edc10f735096e2fb0f3741f0
Medhy Vinceslas <vinceslas.medhy@gmail.com>
2022-11-21 14:40:38 +0100

    Add fully connected deterministic policy network

--- deep_rl/policy_based_and_ac/fc.py
+++ deep_rl/policy_based_and_ac/fc.py
@@ -260,6 +260,68 @@ class FCQV(nn.Module):  # Fully connected Q-function Q(s, a)
             x = self.activation_fc(hidden_layer(x))
 
         return self.output_layer(x)
-    
+
+
+class FCDP(nn.Module):  # fully connected deterministic policy (for continous action)
+    def __init__(
+            self, device, in_dim, action_bounds,
+            hidden_dims=(32,32), activation_fc=F.relu, out_activation_fc=F.tanh):
+        """
+        In the pendulum env, we need to make the difference between a value of -2 and -2.5,
+        that why we use tanh, it allow to map negative strongly negative, 0 to near 0 and so on,
+        so it will map the values between -1 and 1 then we will scale back to original values.
+        """
+        super(FCDP, self).__init__()
+
+        self.device = device
+        self.activation_fc = activation_fc
+        self.out_activation_fc = out_activation_fc
+
+        # min and max value of an action, if we have 2 possible actions [move, jump]
+        # move: values can be in range (-30, 30)
+        # jump: values can be in range (0, 5)
+        # so env_min = [-30, 0] & env_max = [30, 5]
+        self.env_min, self.env_max = action_bounds
+        nA = len(self.env_max)
+
+        self.input_layer = nn.Linear(in_dim, hidden_dims[0])
+        self.hidden_layers = nn.ModuleList()
+
+        for i in range(len(hidden_dims)-1):
+            hidden_layer = nn.Linear(hidden_dims[i], hidden_dims[i+1])
+            self.hidden_layers.append(hidden_layer)
+        
+        self.output_layer = nn.Linear(hidden_dims[-1], nA)
+        self.to(self.device)
+        
+        self.env_min = torch.tensor(self.env_min, device=self.device, dtype=torch.float32)
+        self.env_max = torch.tensor(self.env_max, device=self.device, dtype=torch.float32)
+        
+        # after passing the tanh, outputs will be in range (-1, 1), so we need to scale it back
+        # to original values
+        self.nn_min = self.out_activation_fc(torch.Tensor([float('-inf')])).to(self.device)
+        self.nn_max = self.out_activation_fc(torch.Tensor([float('inf')])).to(self.device)
+        self.rescale_fn = lambda x: (x - self.nn_min) * (self.env_max - self.env_min) / \
+                                    (self.nn_max - self.nn_min) + self.env_min
+
+    def _format(self, state):
+        x = state
+        if not isinstance(x, torch.Tensor):
+            x = torch.tensor(x, device=self.device, dtype=torch.float32)
+            x = x.unsqueeze(0)
+        return x
+
+    def forward(self, state):
+        x = self._format(state)
+        x = self.activation_fc(self.input_layer(x))
+
+        for hidden_layer in self.hidden_layers:
+            x = self.activation_fc(hidden_layer(x))
+
+        x = self.output_layer(x)
+        x = self.out_activation_fc(x)
+        return self.rescale_fn(x)
+
+
 if __name__ == "__main__":
     pass
